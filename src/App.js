3454f6fe80c943b8ef0cf1843f73d892acbafc29
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2024-10-06 02:53:32 +0000

    maintaining a clean structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-//import logo from './logo.svg';
+import React from "react";
 import About from "./components/About";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import contextData from "./context/contextData";
 import ChangeDarkMode from "./hook/ChangeDarkMode";
+
 function App() {
   const darkModeValue = ChangeDarkMode();
 
   return (
     <contextData.Provider value={darkModeValue}>
       <>
-        {/*<Navbar title = "Music" aboutText="About us"/>*/}
         <Navbar title="Text" />
         <div className="container my-3">
           <Textform heading="Enter your text to analyze below" />
